511f2adc871a436a0a3153541438820467c8f242
Kp Adeyinka <>
2021-05-26 16:22:32 +0000

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,9 @@
+const index = require("practical-js-utils")
+
+const life = new index()
+const arr = [1, 2, 3]
+const arr2 = [1, 2, 3]
+
+
+const array = life.array.merge(arr, arr2)
+console.log(array)
